ba48e54abbf20cd8458a42b8dfb4abf0880827c9
hieu2452 <hieu245202@gmail.com>
2023-03-23 17:03:02 +0700

    chi tiet

--- app/src/main/java/com/example/foodypj/src/View/BinhLuanActivity.java
+++ app/src/main/java/com/example/foodypj/src/View/BinhLuanActivity.java
@@ -1,6 +1,7 @@
 package com.example.foodypj.src.View;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,9 +10,13 @@ import com.example.foodypj.R;
 
 
 public class BinhLuanActivity extends AppCompatActivity {
+    TextView txtTenQuanAn, txtDiaChiQuanAn;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_binhluan);
+        txtTenQuanAn = findViewById(R.id.txtTenQuanAn);
+        txtDiaChiQuanAn = findViewById(R.id.txtDiaChiQuanAn);
     }
 }
